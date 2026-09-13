b06801b40d120797cfbf2b3409d89a3f99a352db
xelaxabc-eng <xelaxabc@gmail.com>
2026-09-13 18:19:43 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = `/${url}?` + params;
+    location.href = `${url}.html?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
